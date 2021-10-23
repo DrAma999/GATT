bbbe1b7806443bb3bc8f35f62e4fcf9d5dcd0035
DrAma999 <andrea.alessandro@gmail.com>
2021-10-23 15:15:57 +0200

    Removed platforms and make libraries static

--- Package@swift-5.3.swift
+++ Package@swift-5.3.swift
@@ -1,32 +1,32 @@
 // swift-tools-version:5.3
 import PackageDescription
 
-#if os(Linux)
-let libraryType: PackageDescription.Product.Library.LibraryType = .dynamic
-let supportedPlatforms: [SupportedPlatform]? = nil
-#else
-let libraryType: PackageDescription.Product.Library.LibraryType = .static
-let supportedPlatforms: [SupportedPlatform]? = [
-    // Add support for all platforms starting from a specific version.
-    .macOS(.v10_12),
-    .iOS(.v10),
-    .watchOS(.v3),
-    .tvOS(.v10)
-]
-#endif
+//#if os(Linux)
+//let libraryType: PackageDescription.Product.Library.LibraryType = .dynamic
+//let supportedPlatforms: [SupportedPlatform]? = nil
+//#else
+//let libraryType: PackageDescription.Product.Library.LibraryType = .static
+//let supportedPlatforms: [SupportedPlatform]? = [
+//    // Add support for all platforms starting from a specific version.
+//    .macOS(.v10_12),
+//    .iOS(.v10),
+//    .watchOS(.v3),
+//    .tvOS(.v10)
+//]
+//#endif
 
 let package = Package(
     name: "GATT",
-    platforms: supportedPlatforms,
+//    platforms: supportedPlatforms,
     products: [
         .library(
             name: "GATT",
-            type: libraryType,
+            type: .static,
             targets: ["GATT"]
         ),
         .library(
             name: "DarwinGATT",
-            type: libraryType,
+            type: .static,
             targets: ["DarwinGATT"]
         )
     ],
